f72dbb33be373b4596265f9aca74760eafbe6a81
AIoneKR <64771618+AIoneKR@users.noreply.github.com>
2020-10-23 12:19:35 +0900

    Update Sorago dong.js

--- commands/🎲 게임명령어/Sorago dong.js
+++ commands/🎲 게임명령어/Sorago dong.js
@@ -16,6 +16,6 @@ module.exports = {
     let emojil = client.emojis.cache.get("691681971478462495")//로딩바
 	message.channel.send("질문에 답변을 찾는중..." + `${emojil}`).then(message => {message.delete({ timeout: 1000, reason: 'delete' })})
      setTimeout(function(){ message.channel.send(nember2); }, 2000)
-     console.log(`> ${message.guild.name} < ${message.author.tag} /주사위 사용`)
+     console.log(`> ${message.guild.name} < ${message.author.tag} /소라고동 사용`)
     }
 }
