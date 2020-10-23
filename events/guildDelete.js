35d5ea40896735357296a6a1c2acfe91713fa490
AIoneKR <64771618+AIoneKR@users.noreply.github.com>
2020-10-23 11:02:40 +0900

    Update guildDelete.js

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -3,14 +3,6 @@ const fetch = require('node-fetch');
 const fs = require("fs");
 
 module.exports = async (client, guild) => {
-	let token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3NzM4MTI5MTY2NjE3ODA1OCIsImlhdCI6MTU4ODYwMTI2MCwiZXhwIjoxNjIwMTU4ODYwfQ.HqB1OWLNamYk9yq4wuXi4hpoZIipbQTsG-twqR4gbY6dgA0Zsp_7oDkKrs9oGjMwyy-3rY_OHIiqrNKiTBdF4Kln1UvovQrj-Ogdo5kM1ZDuTwVSqtFTvXGhH7kZ3HFNysy3E0FEdRiBXkHuexUNo7fQv6mOJstd5kUChq4s43s"
-	res = await fetch("https://api.koreanbots.dev/bots/servers", {
-    method: "POST",
-    headers: { token, "Content-type": "application/json" },
-    body: `{ "servers": "${client.guilds.cache.size}" }`
-	}).then(res => res.json())
-	console.log(res)
-
 	console.log(`┌────────Delete server!!────────`)
 	console.log(`│ 서버이름 : ${guild.name}`)
 	console.log(`│ 서버소유자 : ${guild.owner.user.tag}`)
