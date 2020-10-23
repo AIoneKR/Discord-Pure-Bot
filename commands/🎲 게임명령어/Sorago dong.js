697208df7f484b3c1e53a0ae75cda72060477e8a
AIoneKR <64771618+AIoneKR@users.noreply.github.com>
2020-10-23 12:06:14 +0900

    Add files via upload

---
+++ commands/🎲 게임명령어/Sorago dong.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+const blacklists = require("../../Database/blacklist.json");
+
+module.exports = {
+    name: "주사위",
+    category: "🎲 게임명령어",
+    description: "1부터 6까지의 주사위를 랜덤으로 표시합니다.",
+    run: async (client, message, args) => {
+        let nember = ["1️⃣","2️⃣","3️⃣","4️⃣","5️⃣","6️⃣"]
+		let nember2 = nember[Math.floor(Math.random() * nember.length)];
+        
+        message.react('692644452220534857')
+    let emojil = client.emojis.cache.get("691681971478462495")//로딩바
+	message.channel.send("주사위 굴리는중 " + `${emojil}`).then(message => {message.delete({ timeout: 1000, reason: 'delete' })})
+     setTimeout(function(){ message.channel.send(nember2); }, 2000)
+     console.log(`> ${message.guild.name} < ${message.author.tag} /주사위 사용`)
+    }
+}
